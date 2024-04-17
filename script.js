40417800baca60afb17476e18af13cd5510fb844
Rob Winterburn <robertwinterburn@hotmail.co.uk>
2024-04-17 20:31:22 +0100

    added in background for spaceship game may change style though

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ let boardWidth = 360;
 let boardHeight = 640;
 let context; 
 
+//spaceship
+let spaceWidth = 34;
+let spaceHeight = 24;
+let spaceX
+
 window.onload = function(){
   board = document.getElementById("board");
   board.height = boardHeight;
