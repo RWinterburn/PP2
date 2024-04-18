7ccd3b538cf61383cab4dfec8a88ba281ac40ed9
Rob Winterburn <robertwinterburn@hotmail.co.uk>
2024-04-18 11:23:47 +0100

    added height and width for spaceship object within context to the board

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ let context;
 //spaceship
 let spaceWidth = 34;
 let spaceHeight = 24;
-let spaceX
+let spaceX = boardWidth/8;
+let spaceY = boardHeight/2;
+
+let space = {
+  x : spaceX,
+  y : spaceY,
+  width : spaceWidth,
+  height : spaceHeight,
+}
 
 window.onload = function(){
   board = document.getElementById("board");
@@ -15,4 +23,9 @@ window.onload = function(){
   board.width = boardWidth;
   context = board.getContext("2d");
 
+
+
+//spaceship
+context.fillStyle = "red";
+context.fillRect(space.x, space.y, space.width, space.height)
 }
