f65aea7c0ea2f7080290f53d0e68b64819287b9f
Rob Winterburn <robertwinterburn@hotmail.co.uk>
2024-04-19 11:41:59 +0100

    added in top pipe

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context; 
+//pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
 
 //spaceship
 let spaceWidth = 34;
@@ -27,12 +36,26 @@ window.onload = function(){
 
 
 //spaceship
-context.fillStyle = "red";
-context.fillRect(space.x, space.y, space.width, space.height)
+
+
 
 //load images
 spaceImg = new Image();
 spaceImg.src = "./assets/imgs/spaceShip.png";
 spaceImg.onload = function(){
 context.drawImage(spaceImg, space.x, space.y, space.width, space.height);}
+
+topPipeImg = new Image();
+topPipeImg.src = "./assets/imgs/bottompipemod.png";
+
+requestAnimationFrame(update);
+}
+
+function update(){
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+
+  //spaceship
+  context.drawImage(spaceImg, space.x, space.y, space.width, space.height)
+
 }
